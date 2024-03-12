dcbd710066a23c03af6c91c04ba4b57f9ddd3aea
Zachary Sturman <zacharysturman@Zacharys-Laptop.local>
2024-03-13 01:04:01 +1100

    Analytics

--- app/components/nav/NavSlice.tsx
+++ app/components/nav/NavSlice.tsx
@@ -57,6 +57,8 @@ const NavSlice: React.FC<NavSliceProps> = ({ navVisible, setNavVisible }) => {
     <motion.div
       layout
       className="relative bg-slate-300 rounded-full w-48 h-48"
+      animate={navVisible ? "visible" : "hidden"}
+      variants={buttonVariants}
     >
 
       {sheetSections.map((section, index) => (
@@ -70,14 +72,14 @@ const NavSlice: React.FC<NavSliceProps> = ({ navVisible, setNavVisible }) => {
       >
         {section}
 
-        <svg className="absolute top-0 left-0 translate-x-1/2 translate-y-1/2" width={150} height={150} viewBox="0 0 1080 1080" style={{ transform: `rotate(${index * 90-90}deg)` }}>
+        <motion.svg  className="absolute top-0 left-0 translate-x-1/2 translate-y-1/2" width={150} height={150} viewBox="0 0 1080 1080" style={{ transform: `rotate(${index * 90-90}deg)` }}>
         <Path
         props={{
           d: "M881.249 154.288C890.398 143.947 889.467 128.097 878.714 119.437C778.677 38.8636 652.919 -3.58912 523.98 0.237673C395.042 4.06447 272.023 53.9007 176.941 140.265C166.72 149.548 166.73 165.425 176.476 175.205L369.885 369.29C379.631 379.07 395.381 378.965 406.221 370.414C442.435 341.845 487.049 325.476 533.592 324.095C580.135 322.714 625.641 336.408 663.486 362.778C674.814 370.672 690.543 369.843 699.691 359.502L881.249 154.288Z",
           fill: "#788300",
         }}
         />
-      </svg>
+      </motion.svg>
       </motion.button>
       ))}
     </motion.div>
--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { analytics } from "./app/utils/analytics";
 
 export default async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === '/analytics') {
+  if (req.nextUrl.pathname === '/') {
     try {
       await analytics.track('pageview', {
-        page: '/analytics',
+        page: '/',
         country: req.geo?.country,
         region: req.geo?.region,
         city: req.geo?.city,
+        userAgent: req.headers.get('user-agent'),
+        referrer: req.headers.get('referrer')
       });
     } catch (err) {
       console.error(err);
@@ -20,13 +22,8 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/analytics']
-};
-
-
-/* export const config = {
     matcher: [
         `/((?!api|_next/static|_next/image|favicon.ico).*)`
 
     ]
-} */
+} 
